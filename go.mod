d2aac1ee8a14642ed75a4aa58c1c3411f7fa40f7
michaelzhao577 <41598390+michaelzhao577@users.noreply.github.com>
2021-04-01 19:11:13 -0700

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module main.go
+
+go 1.13
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+)
---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+type Response struct {
+	gorm.Model
+
+	Service  string
+	Rating   int
+	Feedback string
+}
+
+var db *gorm.DB
+var err error
+
+func main() {
+	// // load environment variables
+
+	// os.Setenv("HOST", "localhost")
+	// os.Setenv("DBPORT", "5432")
+	// os.Setenv("USER", "postgres")
+	// os.Setenv("NAME", "feedback")
+	// os.Setenv("PASSWORD", "postgres")
+	// host := os.Getenv("HOST")
+	// dbPort := os.Getenv("DBPORT")
+	// user := os.Getenv("USER")
+	// dbName := os.Getenv("NAME")
+	// password := os.Getenv("PASSWORD")
+
+	// // database connection string
+	// dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s", host, user, dbName, password, dbPort)
+
+	dbURI := "postgres://tygpcistkwmqzq:08c0da8860c737732bccc39f990d72a98dfdb955461a224cce28b40af8852214@ec2-107-22-245-82.compute-1.amazonaws.com:5432/dek7e8hfvjod9o"
+
+	// open connection to db
+	db, err = gorm.Open("postgres", dbURI)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Successfully connected to database")
+	}
+
+	// close connection to db when main func terminates
+	defer db.Close()
+
+	// make migration to the db if they have not already been created
+	db.AutoMigrate(&Response{})
+
+	router := handleRequests()
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func getResponses(w http.ResponseWriter, r *http.Request) {
+	var responses []Response
+	db.Find(&responses)
+	json.NewEncoder(w).Encode(responses)
+}
+
+func createResponse(w http.ResponseWriter, r *http.Request) {
+	var response Response
+	json.NewDecoder(r.Body).Decode(&response)
+
+	createdResponse := db.Create(&response)
+	err = createdResponse.Error
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+	} else {
+		json.NewEncoder(w).Encode(&response)
+	}
+}
+
+func handleRequests() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/responses", getResponses).Methods("GET")
+	router.HandleFunc("/create/response", createResponse).Methods("POST")
+
+	return router
+}
